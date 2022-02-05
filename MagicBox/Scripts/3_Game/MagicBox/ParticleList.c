fe02a574057ac143ec79d335f23ccf0d37b1f163
Tyler Paul <tylerpaul2329@gmail.com>
2022-02-05 15:33:00 -0500

    Fixed Aura and updated CrateWeaponEntry to work with 1.16

--- MagicBox/Scripts/3_Game/MagicBox/ParticleList.c
+++ MagicBox/Scripts/3_Game/MagicBox/ParticleList.c
@@ -1,4 +1,4 @@
 modded class ParticleList
 {
-	static const int MYSTERY_BOX_AURA = RegisterParticle("MagicBox/Models/magic_box", "MysteryBox_Aura");
+	static const int MYSTERY_BOX_AURA = RegisterParticle("MagicBox/Models/magic_box/", "MysteryBox_Aura");
 }
--- MagicBox/Scripts/4_World/MagicBox/Classes/CrateWeaponEntry.c
+++ MagicBox/Scripts/4_World/MagicBox/Classes/CrateWeaponEntry.c
@@ -27,7 +27,7 @@ class CrateWeaponEntry
 		
 		Weapon_Base weapon;		
 		if (Class.CastTo(weapon, item) && GetGame().IsKindOf(Mag, "Magazine_Base")) { 
-			AttachMagazine(weapon, Mag);				
+			weapon.SpawnAmmo(Mag);
 		}
 		
 		foreach (string attachment: Attachments) {
@@ -35,68 +35,5 @@ class CrateWeaponEntry
 		}
 		
 		return item;
-	}
-	
-	// https://feedback.bistudio.com/T149240 thanks jacob
-	static void AttachMagazine(Weapon_Base weapon, string magazine_type)
-	{
-		int stateId = -1;
-	
-		if (weapon.IsInherited(SKS_Base)) {
-			return;
-		} 
-		
-		else if (weapon.IsInherited(BoltActionRifle_InnerMagazine_Base)) {
-			return;
-		} 
-		
-		else if (weapon.IsInherited(DoubleBarrel_Base)) {
-			return;
-		} 
-		
-		else if (weapon.IsInherited(Pistol_Base)) {
-			stateId = PistolStableStateID.CLO_DIS_BU0_MA1;
-		} 
-		
-		else if (weapon.IsInherited(CZ527_Base)) {
-			stateId = CZ527StableStateID.CZ527_CLO_BU0_MA1;
-		} 
-		
-		else if (weapon.IsInherited(Repeater_Base)) {
-			return;
-		} 
-		
-		else if (weapon.IsInherited(RifleBoltFree_Base)) {
-			stateId = RBFStableStateID.RBF_CLO_BU0_MA1;
-		} 
-		
-		else if (weapon.IsInherited(RifleBoltLock_Base)) {
-			stateId = RBLStableStateID.RBL_OPN_BU0_MA1;
-		} 
-		
-		else if (weapon.IsInherited(Mp133Shotgun_Base)) {
-			return;
-		}
-	
-		InventoryLocation il = new InventoryLocation;
-		il.SetAttachment(weapon, NULL, InventorySlots.MAGAZINE);
-	
-		// using any of the inventory sync for existing spawning magazines also works
-		// e.g. GameInventory.LocationSyncMoveEntity
-	
-		EntityAI mag = SpawnEntity(magazine_type, il, ECE_IN_INVENTORY, RF_DEFAULT);
-		GetGame().RemoteObjectDelete(mag);
-		GetGame().RemoteObjectDelete(weapon);
-	
-		pushToChamberFromAttachedMagazine(weapon, weapon.GetCurrentMuzzle());
-	
-		ScriptReadWriteContext ctx = new ScriptReadWriteContext;
-		ctx.GetWriteContext().Write(stateId);
-		weapon.LoadCurrentFSMState(ctx.GetReadContext(), GetGame().SaveVersion());
-		
-		GetGame().RemoteObjectCreate(weapon);
-		GetGame().RemoteObjectCreate(mag);
-		
-		//weapon.ValidateAndRepair();
 	}
 }
